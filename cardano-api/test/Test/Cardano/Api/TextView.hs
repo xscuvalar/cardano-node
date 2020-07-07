5712496fba1e9d03fe93c0ed2396dbec41be6793
Jordan Millar <jordan.millar@iohk.io>
2020-07-07 11:20:49 +0100

    Remove more unused functions from Cardano.Api.TextView

--- cardano-api/test/Test/Cardano/Api/TextView.hs
+++ cardano-api/test/Test/Cardano/Api/TextView.hs
@@ -18,20 +18,6 @@ import qualified Hedgehog.Range as Range
 import           Test.Cardano.Config.Gen
 
 
--- Test this first. If this fails, others are likely to fail.
-prop_roundtrip_multiline_hex :: Property
-prop_roundtrip_multiline_hex =
-  Hedgehog.property $ do
-    bs <- BS.pack <$> Hedgehog.forAll (Gen.string (Range.linear 0 500) (Gen.element ['\0' .. '\xff']))
-    Hedgehog.tripping bs (BS.unlines . rawToMultilineHex) unRawToMultilineHex
-
--- Test this second. If this fails, others are likely to fail.
-prop_roundtrip_TextView :: Property
-prop_roundtrip_TextView =
-  Hedgehog.property $ do
-    tv <- Hedgehog.forAll genTextView
-    Hedgehog.tripping tv renderTextView parseTextView
-
 prop_roundtrip_shelley_SigningKey_view :: Property
 prop_roundtrip_shelley_SigningKey_view =
   Hedgehog.property $ do
